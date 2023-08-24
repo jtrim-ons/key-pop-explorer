225914b499d6374cb2f544377b86b19a8c1b5d9c
James Trimble <jtrim.ons@gmail.com>
2023-08-24 21:48:16 +0100

    Work on the OptionPicker

--- src/lib/ui/OptionPicker.svelte
+++ src/lib/ui/OptionPicker.svelte
@@ -1,22 +1,54 @@
 <script>
+  import OptionPickerColumn from "./OptionPickerColumn.svelte";
+
   export let options = [];
   export let catCallback = selectCat;
 
   function selectCat(variable, cat) {
     console.log(variable, cat);
   }
+
+  let selectedColumn = null;
+  let selectedClassification = null;
+  let selectedCategory = null;
 </script>
+
+<div class="container">
+  <OptionPickerColumn
+    columnTitle="Select a variable"
+    bind:selected={selectedColumn}
+    {options}
+  />
+  {#if selectedColumn != null}
+    <OptionPickerColumn
+      columnTitle="Select a classification"
+      labeller={(option) => `${option.cats.length + " categories"}`}
+      bind:selected={selectedClassification}
+      options={selectedColumn.vars}
+    />
+    {#if selectedClassification != null}
+      <OptionPickerColumn
+        columnTitle="Select a category"
+        bind:selected={selectedCategory}
+        options={selectedClassification.cats}
+      />
+    {/if}
+  {/if}
+</div>
 
 <div>
   {#each options as option}
     <details>
       <summary
         style="border: 1px solid lightsteelblue; padding: 3px; width: 300px"
-        >{option.label} ></summary
+        >{option.label}<span class="chevron-right">&gt;</span></summary
       >
       {#each option.vars as v}
         <details style="margin-left: 21px">
-          <summary>{v.cats.length} categories ></summary>
+          <summary
+            >{v.cats.length} categories<span class="chevron-right">&gt;</span
+            ></summary
+          >
           <div style="margin-left: 21px">
             {#each v.cats as c}
               <div>
@@ -34,5 +66,13 @@
   li {
     margin: 0;
     padding: 0;
+  }
+
+  .container {
+    display: flex;
+  }
+
+  .chevron-right {
+    float: right;
   }
 </style>
---
+++ src/lib/ui/OptionPickerColumn.svelte
@@ -0,0 +1,50 @@
+<script>
+  export let options = [];
+  export let clickCallback = clicked;
+  export let columnTitle = "column title";
+  export let labeller = (option) => option.label;
+  export let selected = null;
+  $: console.log(options);
+
+  function clicked(option) {
+    console.log("clicked", option);
+    selected = option;
+  }
+</script>
+
+<div class="column">
+  <h5 class="column-title">{columnTitle}</h5>
+  {#each options as option}
+    <button
+      class:selected={option === selected}
+      on:click={() => clickCallback(option)}
+    >
+      {labeller(option)}
+      <span class="right-chevron">&gt;</span>
+    </button>
+  {/each}
+</div>
+
+<style>
+  .column {
+    width: 33.33%;
+  }
+  button {
+    display: block;
+    width: 100%;
+    background-color: white;
+    text-align: left;
+    border: none;
+    border-bottom: 1px solid #ccc;
+    padding: 3px;
+  }
+  button.selected {
+    background-color: steelblue;
+    color: white;
+  }
+  .column-title {
+  }
+  .right-chevron {
+    float: right;
+  }
+</style>
